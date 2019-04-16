0cbddbe016d2a3850d7f9ec198d769f61d812031
Ryan.S <ryan2002.rs@gmail.com>
2019-04-16 19:11:09 +1000

    Updated Scripts and deleted old ones+cleanup

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ Vanilla Template
 https://templatemo.com/tm-526-vanilla
 
 */
-
+//Parallax and owl carousel init
 jQuery(document).ready(function($) {
 
 	'use strict';
@@ -58,3 +58,42 @@ jQuery(document).ready(function($) {
 
 
 });
+//smooth scrolling
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $(".fixed-side-navbar a, .primary-button a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
+//animate on scroll
+AOS.init();
+//typed.js
+var typedOptions = {
+    strings: ["build websites","make games","take photos","edit media","make web-apps"],
+    typeSpeed: 40,
+    backSpeed: 40,
+    startDelay: 1000,
+    backDelay: 1000,
+    loop: true,
+    loopCount: Infinity,
+    smartBackspace: true
+};
+new Typed("#name", typedOptions);
