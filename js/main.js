b2818a0d0994cf29f9e7e029818d43d770dd8ec8
Ryan.S <ryan2002.rs@gmail.com>
2019-04-29 12:43:46 +1000

    Made Skills Section

--- js/main.js
+++ js/main.js
@@ -26,20 +26,20 @@ jQuery(document).ready(function($) {
       
       // smoothscroll on sidenav click
 
-    $('.tabgroup > div').hide();
-        $('.tabgroup > div:first-of-type').show();
-        $('.tabs a').click(function(e){
-          e.preventDefault();
-            var $this = $(this),
-            tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
-            others = $this.closest('li').siblings().children('a'),
-            target = $this.attr('href');
-        others.removeClass('active');
-        $this.addClass('active');
-        $(tabgroup).children('div').hide();
-        $(target).show();
+    /* $('.tabgroup > div').hide();
+         $('.tabgroup > div:first-of-type').show();
+         $('.tabs a').click(function(e){
+           e.preventDefault();
+             var $this = $(this),
+             tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+             others = $this.closest('li').siblings().children('a'),
+             target = $this.attr('href');
+         others.removeClass('active');
+         $this.addClass('active');
+         $(tabgroup).children('div').hide();
+         $(target).show();
 
-        });
+         });*/
     
     let options={
         
